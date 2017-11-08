d76551783a8a49c1f022aa280f834f469fda31d8
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-11-08 09:31:05 -0600

    POST route and req working, created GET request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,70 @@
 console.log('hello from client.js');
 $(document).ready(onReady);
 
+var editing = false;
+var editingId;
+
 function onReady(){
-    
+    refreshSweaters();
+    $('#submitBtn').on('click', submitBtnClicked);
+    //$('#sweaterList').on('click', '.editBtn', editBtnClicked);
 }
+
+function submitBtnClicked(){
+    var sweaterName = $('#sweaterNameIn').val();
+    var sweaterSize = $('#sizeIn').val();
+    var sweaterPrice = $('#priceIn').val();
+    var sweater = {
+        name: sweaterName,
+        size: sweaterSize,
+        price: sweaterPrice
+    };
+    // clear input fields
+    $('input').val('');
+    if(editing){
+        // switch back to add new product mode
+        editing = false;
+       $('#sweaterSubheading').text('Add New Sweater');
+       //call function that makes PUT request
+       updateSweater(sweater);
+    } else {
+        addNewSweater(sweater);
+    }
+}
+
+//GET request
+function refreshSweaters(){
+    $.ajax({
+        method: 'GET',
+        url: '/sweater'
+    }).done(function(response){
+        console.log('response from GET req:', response);
+        //response is my sweater array
+        var sweaterList = response;
+        //appendSweatersToDom(sweaterList);
+    }).fail(function(error){
+        console.log('something went wrong in GET req:', error); 
+    })
+}
+
+// function updateSweater(sweaterToUpdate) {
+
+// }
+
+// POST a new sweater
+function addNewSweater(sweaterToSend) {
+   $.ajax({
+       method: 'POST',
+       url: '/sweater',
+       data: sweaterToSend
+   }).done(function(response){
+       console.log('response from POST request:', response);
+       refreshSweaters();
+   }).fail(function(error){
+       console.log('something went wrong in POST request:', error); 
+   });
+}
+
+// function editBtnClicked(){
+
+// }
--- server/routes/sweater_router.js
+++ server/routes/sweater_router.js
@@ -1,7 +1,46 @@
 var express = require('express');
 var router = express.Router();
-var sweaters = []; //do I really need this if I have db?
+var sweaters = []; //do I need this if I have db?
 
+var pg = require('pg');
+var config = {
+    database: 'my_practice', //name of the db
+    host: 'localhost', //where is the db?
+    port: 5432, //the port number for the db, 5432 is default
+    max: 10, //how many connections at one time
+    idleTimeoutMillis: 30000 //close idle connections to db after 30 sec
+}
 
+//create pool
+var pool = new pg.Pool(config);
+
+//express removed the '/sweater' when I do an app.use
+router.post('/', function(req, res){
+    var sweater = req.body; //the data sent in the POST request
+    console.log('req.body now sweater:', sweater); //has a name, size, price
+    
+    //Attempt to connect to the database
+    pool.connect(function (errorConnectingToDb, db, done){
+        if(errorConnectingToDb){
+            //there was an error and no connection was made
+            console.log('Error connecting to db:', errorConnectingToDb);
+            res.sendStatus(500);
+        } else {
+            //we connected to the db!! pool -1
+            var queryText = 'INSERT INTO "sweaters" ("name", "size", "price") VALUES ($1, $2, $3);';
+            db.query(queryText, [sweater.name, sweater.size, sweater.price], function (errorMakingQuery, result){
+                //we have received an error or result at this point
+                done(); //pool +1
+                if(errorMakingQuery){
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    //send back success!!
+                    res.sendStatus(201);
+                }
+            }); // END QUERY
+        }
+    }); // END POOL
+});
 
 module.exports = router;
